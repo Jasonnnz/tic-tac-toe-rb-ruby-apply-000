35976dd185343ba5819ac16f136e4aaee37fa94b
bustling-procedure-0781 <jzhou35@buffalo.edu>
2020-10-11 14:13:56 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -44,3 +44,21 @@ def turn(board)
     turn(board)
   end
 end
+
+def turn_count(board)
+  counter = 0 
+  board.each do |elem|
+    if elem == "X" || elem == "O"
+      counter += 1 
+    end
+  end
+  return counter
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0 
+    return "X"
+  else 
+    return "O"
+  end
+end
